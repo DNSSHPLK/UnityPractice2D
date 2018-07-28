4506688a50bf8e2ac3d8d391f255cc4e38dfcefc
DNSSHPLK <39796081+DNSSHPLK@users.noreply.github.com>
2018-07-28 19:41:46 +0300

    Lesson01

---
+++ UnityPractice2D/Assets/Scripts/HeroFollower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollower : MonoBehaviour {
+    public GameObject Player;
+    private void FixedUpdate()
+    {
+        //Получаем доступ к компоненте Transform кролика
+        Transform Player_transform = Player.transform;
+
+        //Получаем доступ к компоненте Transform камеры
+        Transform camera_transform = this.transform;
+
+        //Получаем доступ к координатам кролика
+        Vector3 rabit_position = Player_transform.position;
+        Vector3 camera_position = camera_transform.position;
+
+        //Двигаем камеру только по X,Y
+        camera_position.x = rabit_position.x;
+        camera_position.y = rabit_position.y;
+
+        //Задаем координаты камеры
+        camera_transform.position = camera_position;
+    }
+}
---
+++ UnityPractice2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Rigidbody2D RB;
+    SpriteRenderer SR;    
+    public float speed = 3;
+
+    private void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        SR = GetComponent<SpriteRenderer>();
+    }
+    private void Start()
+    {
+        RB.freezeRotation = true;   
+    }
+    void FixedUpdate()
+    {
+        //[-1, 1]
+        float value = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(value) > 0)
+        {
+            Vector2 vel = RB.velocity;
+            vel.x = value * speed;
+            RB.velocity = vel;
+            if (value < 0)
+            {
+                SR.flipX = true;
+            }
+            else 
+            {
+                SR.flipX = false;
+            }
+        }
+        float value1 = Input.GetAxis("Vertical");
+        if (Mathf.Abs(value1) > 0)
+        {
+            Vector2 vel = RB.velocity;
+            vel.y = value1 * speed;
+            RB.velocity = vel;
+        }
+
+    }
+    private void Update()
+    {
+        Debug.Log(Time.deltaTime);
+    }
+}
